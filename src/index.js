dc578a59e1265cca719216fea4f0b32fbd77bc60
Dara Casey <dara.e.casey.25@dartmouth.edu>
2024-04-10 20:12:04 -0400

    more files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+let time = 0;
+setInterval(() => {
+  time += 1;
+  document.getElementById('main').textContent = `You've been on this page for ${time} seconds.`;
+}, 1000);
